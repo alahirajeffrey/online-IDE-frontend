766e9aa9cfc02c8253359e467153eb4f3b2fc440
alahirajeffrey <alahirajeffrey@gmail.com>
2024-06-20 08:16:09 +0100

    added application routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Register from "./components/Register";
+import Login from "./components/Login";
+import Problems from "./components/Problems";
+import Solution from "./components/Solution";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/problems" element={<Problems />} />
+        <Route path="/problems/:problemId" element={<Solution />} />
+      </Routes>
+    </Router>
   );
 }
 
